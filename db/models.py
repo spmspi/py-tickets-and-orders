78f60b2d8f80b50c28958c59b140a864cbbf3948
SPMSPI <kislicin.artur@gmail.com>
2026-04-23 07:25:43 +0300

    I edited it 3 add " in models str metod

--- db/models.py
+++ db/models.py
@@ -61,8 +61,8 @@ class MovieSession(models.Model):
     )
 
     def __str__(self) -> str:
-        return f"{self.movie.title} {
-        self.show_time.strftime('%Y-%m-%d %H:%M:%S')}"
+        return (f"{self.movie.title} "
+                f"{self.show_time.strftime('%Y-%m-%d %H:%M:%S')}")
 
 
 class Order(models.Model):
